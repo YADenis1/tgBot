373e54fe57b453420b40f6695da91a59b74c0787
YADenis1 <krivosheindenis9@gmail.com>
2024-10-11 00:46:57 +0600

    first commit

---
+++ bot.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <tgbot/tgbot.h>
+using namespace TgBot;
+using namespace std;
+
+int main() {
+    TgBot::Bot bot("5754837263:AAErExq127fiwhAvTB5h2-YPgEZyDA7a92k");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            bot.getApi().sendMessage(message->chat->id, "Bot working!");
+            return;
+        }
+        if (StringTools::startsWith(message->text, "/day")) {
+            bot.getApi().sendPhoto(message->chat->id, InputFile::fromFile("1.png", "image/png"));
+            return;
+        }
+        });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
